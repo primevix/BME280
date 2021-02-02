7e97077e5647fa1eaf48f36f45f06c98568c0707
Primevix <jumerle19@gmail.com>
2021-02-02 17:00:19 -0300

    Update bme280.ino

--- bme280.ino
+++ bme280.ino
@@ -2,7 +2,7 @@
 #include <BlynkSimpleEsp8266.h>
       
 #include <Wire.h>
-#include "SPI.h" //Why? Because library supports SPI and I2C connection
+#include "SPI.h"
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 
@@ -16,27 +16,27 @@ char server[] = "*********";
 BlynkTimer timer;
 
 //Variables
-float pressure;     //To store the barometric pressure (Pa)
-float temperature;  //To store the temperature (oC)
-int altimeter;      //To store the altimeter (m) (you can also use it as a float variable)
+float pressure;
+float temperature;
+int altimeter;
 
 void setup() {
-  bmp.begin();    //Begin the sensor
-  Serial.begin(9600); //Begin serial communication at 9600bps
+  bmp.begin();
+  Serial.begin(9600);
   Serial.println("Adafruit BMP280 test:");
   Blynk.begin(auth, ssid, pass, server);
-  timer.setInterval(5000L, ReadSensors);   // read sensor every 5s 
+  timer.setInterval(5000L, ReadSensors);
 }
 
   void ReadSensors(){
   //Read values from the sensor:
   pressure = bmp.readPressure();
   temperature = bmp.readTemperature();
-  altimeter = bmp.readAltitude (1029.00); //Change the "1050.35" to your city current barrometric pressure (https://www.wunderground.com)
+  altimeter = bmp.readAltitude (1029.00);
 
-  Blynk.virtualWrite(V1, pressure/100);     // write pressure to V1 value display widget
-  Blynk.virtualWrite(V2, temperature);  // write temperature to V2 value display widget
-  Blynk.virtualWrite(V3, altimeter-51);    // write altimeter to V3 value display widget
+  Blynk.virtualWrite(V1, pressure/100);
+  Blynk.virtualWrite(V2, temperature);
+  Blynk.virtualWrite(V3, altimeter-51);
       
   //Print values to serial monitor:
   Serial.print(F("Pressure: "));
@@ -48,9 +48,9 @@ void setup() {
   Serial.print(" oC");
   Serial.print("\t");
   Serial.print("Altimeter: ");
-  Serial.print(altimeter); // this should be adjusted to your local forcase
+  Serial.print(altimeter);
   Serial.println(" m");    
-  //delay(5000); //Update every 5 sec  
+  //delay(5000);
 }
 
 void loop() {
